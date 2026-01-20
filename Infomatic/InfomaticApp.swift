aa67f144acb97fac638b4726baf6886e78f06cdc
rrtyagii <rtyagi1@hawk.iit.edu>
2026-01-19 23:35:30 -0600

    initial commit

---
+++ Infomatic/InfomaticApp.swift
@@ -0,0 +1,17 @@
+//
+//  InfomaticApp.swift
+//  Infomatic
+//
+//  Created by Rishabh Tyagi on 1/19/26.
+//
+
+import SwiftUI
+
+@main
+struct InfomaticApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
